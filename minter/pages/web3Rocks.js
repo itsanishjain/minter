48aae28daa2665a2f8e262d74da5d63df6cee4e1
anish jain <helloanishjain@gmail.com>
2022-03-03 19:32:11 +0530

    added everything

--- minter/pages/web3Rocks.js
+++ minter/pages/web3Rocks.js
@@ -8,34 +8,50 @@ import Card from "../src/components/Card";
 import Button from "../src/components/Button";
 import Loader from "../src/components/Loader";
 
-import { abi, NFT_CONTRACT_ADDRESS } from "../src/constants";
 import Form from "../src/components/Form";
+
+import { NFT_CONTRACT_ADDRESS, NFT_CONTRACT_ABI } from "../src/constants";
 
 export default function Web3Rocks() {
   const [walletConnected, setWalletConnected] = useState(false);
 
   const [loading, setLoading] = useState(false);
   const [tokenIdsMinted, setTokenIdsMinted] = useState("0");
+
+  const [metadataURLIPFS, setMetadataURLIPFS] = useState("");
+
   const [nftUrl, setNftUrl] = useState(false);
 
   const web3ModalRef = useRef();
 
   const publicMint = async () => {
-    try {
-      const signer = await getProviderOrSigner(true);
+    if (metadataURLIPFS !== "") {
+      try {
+        const signer = await getProviderOrSigner(true);
 
-      const whitelistContract = new Contract(NFT_CONTRACT_ADDRESS, abi, signer);
+        const NFTeeContract = new Contract(
+          NFT_CONTRACT_ADDRESS,
+          NFT_CONTRACT_ABI,
+          signer
+        );
 
-      const tx = await whitelistContract.mint({
-        value: utils.parseEther("0.01"),
-      });
-      setLoading(true);
-      await tx.wait();
-      setLoading(false);
-      setNftUrl(true);
-      toast("You successfully minted a web3Rocks!");
-    } catch (err) {
-      console.error(err);
+        const tx = await NFTeeContract.mintNFT(metadataURLIPFS);
+
+
+        setLoading(true);
+        await tx.wait();
+
+         
+
+
+        setLoading(false);
+        setNftUrl(true);
+        toast("You successfully minted a web3Rocks!");
+      } catch (err) {
+        console.error(err);
+      }
+    } else {
+      toast("Please upload an image");
     }
   };
 
@@ -51,7 +67,11 @@ export default function Web3Rocks() {
   const getTokenIdsMinted = async () => {
     try {
       const provider = await getProviderOrSigner();
-      const nftContract = new Contract(NFT_CONTRACT_ADDRESS, NFT_CONTRACT_ABI, provider);
+      const nftContract = new Contract(
+        NFT_CONTRACT_ADDRESS,
+        NFT_CONTRACT_ABI,
+        provider
+      );
       const _tokenIds = await nftContract.tokenIds();
       setTokenIdsMinted(_tokenIds.toString());
     } catch (err) {
@@ -74,6 +94,8 @@ export default function Web3Rocks() {
     }
     return web3Provider;
   };
+
+  console.log("setMetadataURLIPFS!!##################333!", metadataURLIPFS);
 
   useEffect(() => {
     if (!walletConnected) {
@@ -106,24 +128,22 @@ export default function Web3Rocks() {
       <ToastContainer />
 
       <Card
-        name="Welcome to Web3 Rocks!"
-        designation={`Its an NFT collection for developers in Web3 ${tokenIdsMinted}/20 have been minted `}
+        name="Welcome to Clever Programmer!"
+        designation={`Its an NFT Minter for clever developers `}
         imageUrl="rocket.svg"
       />
       {nftUrl && (
         <div className="text-center mt-4">
           <a
             className="text-lg font-medium  text-black cursor-pointer underline"
-            href={`https://testnets.opensea.io/assets/${NFT_CONTRACT_ADDRESS}/${tokenIdsMinted}`}
+            href={`https://testnets.opensea.io/assets/${NFT_CONTRACT_ADDRESS}/${1}`}
           >
             Here is your NFT
           </a>
         </div>
       )}
 
-      
-        
-      <Form />
+      <Form setMetadataURLIPFS={setMetadataURLIPFS} />
 
       <div className="max-w-sm mx-auto text-center mt-8 text-xl font-medium ">
         {renderButton()}
--- minter/src/components/Form.js
+++ minter/src/components/Form.js
@@ -1,7 +1,15 @@
 import React from "react";
 import { useState } from "react";
+import {
+  pinataUploadImage,
+  pinataUploadMetadata,
+  nftDotStorage,
+} from "../../utils";
+import Loader from "./Loader";
 
-export default function Form() {
+export default function Form({setMetadataURLIPFS}) {
+  const [loading, setLoading] = useState(false);
+
   const [formValues, setFormValues] = useState({
     imageUrl: "",
   });
@@ -11,10 +19,11 @@ export default function Form() {
   const validateForm = (_formValues) => {
     const errors = {};
     if (!_formValues.imageUrl) {
-      errors.companyName = "please upload an image";
+      errors.imageUrl = "please upload an image";
     }
     return errors;
   };
+
   // create a function which set the values of form field
   const handleOnChange = (e) => {
     setFormValues({ ...formValues, [e.target.name]: e.target.value });
@@ -22,40 +31,46 @@ export default function Form() {
 
   const handleFileInput = (e) => {
     setFormValues({ ...formValues, [e.target.name]: e.target.files[0] });
-
-    // console.log(e.target.files[0].src,">>>>>>>>>");
-    var file = e.target.files[0];
-    var reader = new FileReader();
-    var url = reader.readAsDataURL(file);
-
-    console.log("IMG", url);
   };
 
-  const handleSubmit = (e, obj) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     let _errors = validateForm(formValues);
     setFormErrors(_errors);
     if (Object.keys(_errors).length === 0) {
-      console.log("NOOOOO ERRORS");
-      console.log(formValues.imageUrl);
+      console.log("NO ERRORs");
       setLoading(true);
-      nftDotStorage(formValues.imageUrl).then((contentId) => {
-        // console.log("CONTENT ID", contentId);
-        createPost(contentId);
-      });
-      console.log("FORM SUBMITED SUCCESSFULLY");
+      const metadata = await nftDotStorage(formValues.imageUrl);
+      console.log("METADATA", metadata.url);
+      setMetadataURLIPFS(metadata.url);
+      setLoading(false);
+      console.log("UPLOADED IMAGE TO IPFS");
     }
   };
 
   return (
-    <div className="max-w-lg mx-auto mt-8 border border-red-600">
+    <div className="max-w-lg mx-auto mt-8 border border-red-600 p-4">
       <form>
+        <p className="text-red-800 font-bold text-md">
+          {formErrors.imageUrl && "Please upload an image"}
+        </p>
         <input
+          className="mt-2"
           type="file"
           name={Object.keys(formValues)[0]}
           onChange={handleFileInput}
         ></input>
       </form>
+      {!loading ? (
+        <button
+          className="mt-2 border border-blue-500 p-4 rounded-lg"
+          onClick={handleSubmit}
+        >
+          UPLOAD
+        </button>
+      ) : (
+        <Loader />
+      )}
     </div>
   );
 }
